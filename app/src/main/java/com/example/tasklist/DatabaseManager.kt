5ea1d9e1b12127ecca84444176995026478a25ee
ivan <ivanroaprieto@gmail.com>
2025-03-26 12:36:55 +0100

    first part

---
+++ app/src/main/java/com/example/tasklist/DatabaseManager.kt
@@ -0,0 +1,10 @@
+package com.example.tasklist
+
+import android.content.Context
+import android.content.SyncContext
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseManager(context: Context) : SQLiteOpenHelper(Context,) {
+
+
+}
