c07b9f9f30816b2e824a8b9b86dfed30bb4ef2d2
ivan <ivanroaprieto@gmail.com>
2025-03-27 11:00:45 +0100

    implement sqlite and add task table with DAO

--- app/src/main/java/com/example/tasklist/Task.kt
+++ app/src/main/java/com/example/tasklist/Task.kt
@@ -1,4 +1,17 @@
 package com.example.tasklist
 
-class Task {
+class Task (
+    var id: Long,
+    var title: String,
+    var done: Boolean
+) {
+
+
+    companion object {
+        const val TABLE_NAME = "Tasks"
+
+        const val COLUMN_NAME_ID = "id"
+        const val COLUMN_NAME_TITLE = "title"
+        const val COLUMN_NAME_DONE = "done"
+    }
 }
